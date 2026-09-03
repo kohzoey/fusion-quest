c7b023a0f4a556b4db7a60dbf195daf90cd57c61
kohzoey <kohzoey@1utar.my>
2026-09-03 16:52:50 +0800

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
 import { PageFrame } from "./PageFrame";
+import { ScrollToTop } from "./ScrollToTop";
 
 export function Layout() {
   return (
     <>
+      <ScrollToTop />
       <a href="#main-content" className="visually-hidden">
         Skip to main content
       </a>
